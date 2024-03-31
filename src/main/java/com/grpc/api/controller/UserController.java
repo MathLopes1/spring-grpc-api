a08b4bacf31816ab352170b1549a70a186969873
MathLopes1 <iamatheuslopes@gmail.com>
2024-03-31 11:46:36 -0300

    feature(grpc-server): criação de listagem deusuario via servidor grpc :rocket:

--- src/main/java/com/grpc/api/controller/UserController.java
+++ src/main/java/com/grpc/api/controller/UserController.java
@@ -7,6 +7,9 @@ import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @GrpcService
 public class UserController extends UserServiceGrpc.UserServiceImplBase {
 
@@ -35,7 +38,20 @@ public class UserController extends UserServiceGrpc.UserServiceImplBase {
 
     @Override
     public void getAll(EmptyReq request, StreamObserver<UserResList> responseObserver) {
-        super.getAll(request, responseObserver);
+        List<User> users = userRepository.findAll();
+
+        List<UserRes> usersRes = users.stream().map(user -> UserRes.newBuilder()
+                    .setId(user.getId())
+                    .setName(user.getName())
+                    .setEmail(user.getEmail())
+                    .build())
+                .toList();
+
+        UserResList userResList = UserResList.newBuilder().addAllUsers(usersRes).build();
+
+        responseObserver.onNext(userResList);
+
+        responseObserver.onCompleted();
     }
 
     @Override
